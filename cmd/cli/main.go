146138ac8fa53d19f04c0621f87a03b7f291c75a
Reinami <typhonnge@gmail.com>
2025-04-19 17:46:30 -0700

    cli wip

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,1 +1,129 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/Reinami/configprovider/cryptography"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		showHelp()
+		return
+	}
+
+	command := os.Args[1]
+
+	fs := flag.NewFlagSet(command, flag.ExitOnError)
+
+	var listAlgo, algorithm, secret, value string
+	fs.StringVar(&listAlgo, "l", "", "Lists all available algorithms")
+	fs.StringVar(&listAlgo, "list-algorithms", "", "Lists all available algorithms")
+	fs.StringVar(&algorithm, "c", "", "Crypto Algorithm (run --l to see a list of supported Algorithms")
+	fs.StringVar(&algorithm, "crypto-algorithm", "", "Crypto Algorithm (run --l to see a list of supported Algorithms")
+	fs.StringVar(&secret, "s", "", "Your secret key for encryption/decryption")
+	fs.StringVar(&secret, "secret-key", "", "Your secret key for encryption/decryption")
+	fs.StringVar(&value, "v", "", "Value to encrypt/decrypt")
+	fs.StringVar(&value, "value", "", "Value to encrypt/decrypt")
+
+	err := fs.Parse(os.Args[2:])
+	if err != nil {
+		panic(err)
+	}
+
+	args := fs.Args()
+	if len(args) >= 2 {
+		if secret == "" {
+			secret = args[0]
+		}
+
+		if value == "" {
+			value = args[1]
+		}
+	}
+
+	if algorithm == "" || secret == "" || value == "" {
+		fmt.Println("Error: --crypto-algorithm, secret, and value are required")
+		showHelp()
+		return
+	}
+
+	switch command {
+	case "encrypt":
+		encryptSecret(algorithm, secret, value)
+	case "decrypt":
+		decryptSecret(algorithm, secret, value)
+	case "--help", "-h", "help":
+		showHelp()
+	default:
+		fmt.Println("unknown command %v", command)
+		showHelp()
+	}
+}
+
+func encryptSecret(algorithm string, secret string, value string) {
+	algo, err := getAlgorithm(algorithm, secret)
+	if err != nil {
+		printErr(err)
+		return
+	}
+
+	encryptedValue, err := algo.Encrypt(value)
+	if err != nil {
+		printErr(err)
+		return
+	}
+
+	fmt.Println(encryptedValue)
+}
+
+func decryptSecret(algorithm string, secret string, value string) {
+	algo, err := getAlgorithm(algorithm, secret)
+	if err != nil {
+		printErr(err)
+		return
+	}
+
+	decryptedValue, err := algo.Decrypt(value)
+	if err != nil {
+		printErr(err)
+		return
+	}
+
+	fmt.Println(decryptedValue)
+}
+
+func getAlgorithm(algorithm string, secret string) (cryptography.CryptoAlgorithm, error) {
+	switch algorithm {
+	case "aesgcm":
+		return cryptography.NewAESGCMCrypto(secret)
+	}
+
+	return nil, fmt.Errorf("unsupported algorthim, %v", algorithm)
+}
+
+func showHelp() {
+	fmt.Println(`Usage:
+  lockbox <command> [options] <secret> <value>
+
+Commands:
+  encrypt     Encrypt a value
+  decrypt     Decrypt a value
+
+Options:
+  --c, --crypto-algorithm   Required. The crypto algorithm to use (e.g., aesgcm)
+  --s, --secret-key         Optional. Secret key (can be passed positionally)
+  --v, --value              Optional. Value to encrypt/decrypt (can be passed positionally)
+  --l, --list-algorithms    Optional. Will display a list of currently supported algorithms
+
+Examples:
+  lockbox encrypt --c=aesgcm mysecret myvalue
+  lockbox decrypt --c=aesgcm mysecret myencryptedvalue`)
+}
+
+func printErr(err error) {
+	fmt.Println("%v", err)
+	showHelp()
+}
--- configprovider/crypto.go
+++ configprovider/crypto.go
@@ -12,6 +12,11 @@ type Decrypter interface {
 	Decrypt(cipherText string) (string, error)
 }
 
+type CryptoAlgorithm interface {
+	Decrypter
+	Encrypter
+}
+
 func decryptValue(key string, value string, decrypter Decrypter) (string, error) {
 	if decrypter == nil {
 		return "", fmt.Errorf("no decrypter is provided")
